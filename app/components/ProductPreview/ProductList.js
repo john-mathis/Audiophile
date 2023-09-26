0b430c24c01a01c05322fc1f0967a002670f1e0f
John Mathis <johnmathis98@gmail.com>
2023-09-26 10:38:47 -0400

    product preview edits

--- app/components/ProductPreview/ProductList.js
+++ app/components/ProductPreview/ProductList.js
@@ -1,6 +1,9 @@
 import HeadphoneOne from "../../../public/assets/product-xx99-mark-two-headphones/mobile/image-product.jpg";
 import HeadphoneTwo from "../../../public/assets/product-xx99-mark-one-headphones/mobile/image-product.jpg";
 import HeadphoneThree from "../../../public/assets/product-xx59-headphones/mobile/image-product.jpg";
+import SpeakerOne from "../../../public/assets/product-zx9-speaker/mobile/image-product.jpg";
+import SpeakerTwo from "../../../public/assets/product-zx7-speaker/mobile/image-product.jpg";
+import EarphoneOne from "../../../public/assets/product-yx1-earphones/mobile/image-product.jpg";
 
 const ListItems = [
   {
@@ -8,7 +11,8 @@ const ListItems = [
     category: "Headphones",
     img: HeadphoneOne,
     title: "XX99 Mark II Headphones",
-    desription:
+    new: "New Product",
+    description:
       "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
   },
   {
@@ -16,7 +20,8 @@ const ListItems = [
     category: "Headphones",
     img: HeadphoneTwo,
     title: "XX99 Mark I Headphones",
-    desription:
+    new: null,
+    description:
       "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
   },
   {
@@ -24,32 +29,36 @@ const ListItems = [
     category: "Headphones",
     img: HeadphoneThree,
     title: "XX59 Headphones",
-    desription:
+    new: null,
+    description:
       "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move. ",
   },
 
   {
     id: 4,
     category: "Speakers",
-    img: HeadphoneThree,
+    img: SpeakerOne,
     title: "ZX9 speaker",
-    desription:
+    new: null,
+    description:
       "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups. ",
   },
   {
     id: 5,
     category: "Speakers",
-    img: HeadphoneThree,
+    img: SpeakerTwo,
     title: "ZX7 SPEAKER",
-    desription:
+    new: null,
+    description:
       "Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
   },
   {
     id: 6,
     category: "Earphones",
-    img: HeadphoneThree,
+    img: EarphoneOne,
     title: "YX1 WIRELESS EARPHONES",
-    desription:
+    new: null,
+    description:
       "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature. ",
   },
 ];
--- app/components/ProductPreview/ProductPreview.tsx
+++ app/components/ProductPreview/ProductPreview.tsx
@@ -10,10 +10,17 @@ const ProductPreview: React.FC<ProductPreviewProps> = ({ props }) => {
     <section>
       {props.map((info: any) => {
         return (
-          <div>
-            <p>{info.title}</p>
-            <p className="text-center">New Product</p>
-            <CtaButton bgColor="bg-red-500" textColor="white" />
+          <div className="mb-24 w-80 m-auto">
+            <img className="mb-6" src={info.img.src} />
+            <p className="text-center uppercase text-[--light-orange] mb-6 tracking-widest">
+              {info.new ? info.new : ""}
+            </p>
+            <img src={info.img} alt="" />
+            <p className="uppercase text-center text-2xl font-bold">
+              {info.title}
+            </p>
+            <p className="text-center mt-4">{info.description}</p>
+            <CtaButton bgColor="bg-[--dark-orange]" textColor="white" />
             <p></p>
           </div>
         );
