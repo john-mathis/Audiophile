9be982f8326dd18998b4033abf138a8dc5d5488d
John Mathis <johnmathis98@gmail.com>
2023-10-01 17:52:33 -0400

    best audio edits

--- app/components/BestAudio/BestAudio.tsx
+++ app/components/BestAudio/BestAudio.tsx
@@ -3,14 +3,18 @@ import BestGearImage from "../../../public/assets/shared/mobile/image-best-gear.
 
 const BestAudio = () => {
   return (
-    <section className="w-80 md:w-[43rem]  m-auto">
-      <img className="m-auto rounded-md" src={BestGearImage.src} alt="" />
-      <div className="md:w-[35rem] mx-auto md:my-12">
-        <p className="uppercase text-3xl md:text-4xl text-center my-4 font-semibold">
+    <section className="w-80 md:w-[43rem] m-auto lg:flex lg:w-[60rem] xl:w-[69rem]">
+      <img
+        className="m-auto rounded-md order-2 lg:text-left lg:w-[33rem]"
+        src={BestGearImage.src}
+        alt=""
+      />
+      <div className="md:w-[35rem] mx-auto md:my-12 order-1 text-center lg:text-left lg:p-8">
+        <p className="uppercase text-3xl md:text-4xl my-4 font-semibold">
           Bringing you the <span className="text-[--dark-orange]">best </span>
           audio gear
         </p>
-        <p className="text-center">
+        <p>
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
