8824ac2ab6f0a9fa36ab54b212d03e0437df4b96
John Mathis <johnmathis98@gmail.com>
2023-10-01 20:53:56 -0400

    added comments to top navbar component

--- app/components/Navbar/TopNavbar.tsx
+++ app/components/Navbar/TopNavbar.tsx
@@ -16,20 +16,30 @@ const TopNavbar: React.FC<NavbarProps> = ({
 }) => {
   return (
     <nav className="flex justify-between items-center text-white bg-black h-14 p-6">
+
+      
+      {/* Responsible for opening the navbar menu */}
       <GiHamburgerMenu
         className="h-6 w-5 cursor-pointer lg:hidden"
         onClick={() => {
           setIsSliderOpen(!isSliderOpen);
         }}
       />
+
+      {/* Audiophile heading */}
       <h1 className="font-extrabold text-xl md:flex cursor-pointer">
         audiophile
       </h1>
+
+      {/*Displays every item in the NavItems array as a navbar link */}
       <ul className="text-white justify-between w-[30rem] hidden sm:hidden md:hidden lg:flex">
         {NavItems.map((items) => {
           return (
-            <Link href={items === "home" ? "/" : items}>
+            <Link href={items}>
               <li
+                onClick={() => {
+                  console.log(isSliderOpen);
+                }}
                 key={items}
                 className="uppercase cursor-pointer hover:text-[--dark-orange]"
               >
@@ -40,6 +50,7 @@ const TopNavbar: React.FC<NavbarProps> = ({
         })}
       </ul>
 
+      {/*Shopping cart icon  */}
       <AiOutlineShoppingCart className="h-6 w-5 cursor-pointer hover:text-[--dark-orange]" />
     </nav>
   );
