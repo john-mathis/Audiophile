245a4c10df930750c657084feb1482b25904df58
John Mathis <johnmathis98@gmail.com>
2023-10-07 18:52:43 -0400

    footer edits

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,31 +7,42 @@ import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <section className="bg-black text-white px-6 py-10 text-center mt-24">
-      <p>audiophile </p>
-      <div className="flex flex-col ">
+    <section className="bg-black text-white px-6 py-10 text-center md:text-left mt-24">
+      <p className="text-2xl font-bold mb-8 border-t-2">audiophile </p>
+      <div className="flex flex-col md:flex-row justify-between md:w-[26rem]	 ">
         {navItems.map((items) => (
-          <Link className="my-2" href={items === "home" ? "/" : items}>
+          <Link
+            className="my-2 font-bold"
+            href={items === "home" ? "/" : items}
+          >
             {items.toLocaleUpperCase()}
           </Link>
         ))}
       </div>
-      <p className="text-base w-[20rem] ">
+      <p className="my-8 text-base">
         Audiophile is an all in one stop to fulfill your audio needs. We're a
         small team of music lovers and sound specialists who are devoted to
         helping you get the most out of personal audio. Come and visit our demo
         facility - we’re open 7 days a week.
       </p>
 
-      <p>Copyright 2021. All Rights Reserved</p>
+      <div className="md:flex items-center md:justify-between md:mt-20">
+        <p className="font-bold">Copyright 2021. All Rights Reserved</p>
 
-      <div>
-        <AiFillFacebook />
-        <AiOutlineTwitter />
-        <AiOutlineInstagram />
+        <div className="flex mt-6 md:mt-0 m-auto justify-center">
+          {footerIcons.map((icon) => (
+            <span className="text-3xl mx-2 md:flex">{icon}</span>
+          ))}
+        </div>
       </div>
     </section>
   );
 };
+
+const footerIcons = [
+  <AiFillFacebook />,
+  <AiOutlineTwitter />,
+  <AiOutlineInstagram />,
+];
 
 export default Footer;
