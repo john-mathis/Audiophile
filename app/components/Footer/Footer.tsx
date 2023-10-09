fcfe9a310fbd84b12851fe7bba8ad349402abff5
John Mathis <johnmathis98@gmail.com>
2023-10-09 17:21:20 -0400

    footer edits

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,34 +8,40 @@ import { AiOutlineInstagram } from "react-icons/ai";
 const Footer = () => {
   return (
     <section className="relative bg-black text-white px-6 py-10 text-center md:text-left mt-24">
-      <div className="border-t-4 w-24 mx-auto md:mx-0 absolute top-0 left-0 md:left-6 right-0 border-[--dark-orange]"></div>
-      <div className="lg:flex lg:justify-between">
-        <p className="text-2xl font-bold mb-8">audiophile </p>
-        <div className="flex flex-col md:flex-row justify-between md:w-[26rem]	 ">
-          {navItems.map((items) => (
-            <Link
-              className="my-2 font-bold"
-              href={items === "home" ? "/" : items}
-            >
-              {items.toLocaleUpperCase()}
-            </Link>
-          ))}
+      <div className="max-w-[69rem] xl:m-auto">
+        <div className="border-t-4 w-24 mx-auto md:mx-0 absolute top-0 left-0 md:left-6  right-0 border-[--dark-orange]"></div>
+
+        <div className="lg:flex lg:justify-between lg:items-center">
+          <p className="text-2xl font-bold mb-8 lg:mb-0">audiophile </p>
+
+          {/* Maps over and displays each navbar item.   */}
+          <div className="flex flex-col md:flex-row justify-between md:w-[26rem]">
+            {navItems.map((items) => (
+              <Link
+                className="my-2 font-bold"
+                href={items === "home" ? "/" : items}
+              >
+                {items.toLocaleUpperCase()}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-      <p className="my-8 text-base md:w-[46rem]">
-        Audiophile is an all in one stop to fulfill your audio needs. We're a
-        small team of music lovers and sound specialists who are devoted to
-        helping you get the most out of personal audio. Come and visit our demo
-        facility - we’re open 7 days a week.
-      </p>
+        <p className="my-8 text-base md:w-[46rem]">
+          Audiophile is an all in one stop to fulfill your audio needs. We're a
+          small team of music lovers and sound specialists who are devoted to
+          helping you get the most out of personal audio. Come and visit our
+          demo facility - we’re open 7 days a week.
+        </p>
 
-      <div className="md:flex items-center md:justify-between md:mt-20">
-        <p className="font-bold">Copyright 2021. All Rights Reserved</p>
+        <div className="md:flex items-center md:justify-between md:mt-20">
+          <p className="font-bold">Copyright 2021. All Rights Reserved</p>
 
-        <div className="flex mt-6 md:mt-0 m-auto md:m-0 justify-center">
-          {footerIcons.map((icon) => (
-            <span className="text-3xl mx-2 md:flex">{icon}</span>
-          ))}
+          {/* Maps over and displays each social media icon */}
+          <div className="flex mt-6 md:mt-0 m-auto md:m-0 justify-center">
+            {footerIcons.map((icon) => (
+              <span className="text-3xl mx-2 md:flex">{icon}</span>
+            ))}
+          </div>
         </div>
       </div>
     </section>
